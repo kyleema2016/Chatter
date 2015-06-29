ff439a5e61b011579b8a555c82cf1261df40559c
antmx <antmx1@ntlworld.com>
2015-06-29 23:46:20 +0100

    Initial check-in

---
+++ Server/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Chatter.Server
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
---
+++ Server/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chatter.Server.WebApiApplication" Language="C#" %>
