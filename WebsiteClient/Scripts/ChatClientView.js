18b49ad29e33a685f2e12c39e172253d62b5ff70
Anthony Chambers <antmx1@ntlworld.com>
2015-07-27 12:04:02 +0100

    Create ChatClientView.js

---
+++ WebsiteClient/Scripts/ChatClientView.js
@@ -0,0 +1,11 @@
+
+function ChatClientView() {
+
+    this.userNameInput = $("#user-name");
+    this.joinButton = $("#join-user");
+    this.infoLabel = $("#info");
+    this.messageInput = $("#message");
+    this.sendMessageButton = $("#send-message");
+    this.requestActiveUsersButton = $("#request-active-users");
+    this.discussionList = $("#discussion-list");
+}
